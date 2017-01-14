52a1ef991067f1faa80b3ecfd02edacec3bc639e
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2017-01-14 18:50:00 +0900

    [Setup] - build process @heroku

---
+++ webpack.production.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: __dirname + '/src/index.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          comments: false,
+          compact: true,
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.[s]?css$/,
+        loaders: ['style', 'css?modules&localIdentName=[path][name]---[local]---[hash:base64:5]', 'sass'],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+};
